68e3361d9383b2ca51775a01de0c8444882c930a
Thomas M <zaz22@txstate.edu>
2024-09-24 21:56:32 -0500

    created xcode project in bitbucket repo

---
+++ fitlife/fitlifeTests/fitlifeTests.swift
@@ -0,0 +1,17 @@
+//
+//  fitlifeTests.swift
+//  fitlifeTests
+//
+//  Created by Thomas Mendoza on 9/24/24.
+//
+
+import Testing
+@testable import fitlife
+
+struct fitlifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
