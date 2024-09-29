ad593cedb5d411aafa2fa8cf6abe87d54cb989f0
Luke Flannigan <nym15@txstate.edu>
2024-09-29 02:55:21 -0500

    Enhanced UI for SignIn and SignUp views and added navigation.

--- fitlife/fitlife/Views/SignInView.swift
+++ fitlife/fitlife/Views/SignInView.swift
@@ -6,32 +6,69 @@ import SwiftUI
 struct SignInView: View {
     @State private var email = ""
     @State private var password = ""
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         ZStack {
             GradientBackground()
             
-            VStack(spacing: 30) {
-                Text("Welcome Back")
-                    .font(.system(size: 32, weight: .bold))
-                    .foregroundColor(.white)
-                
-                TextField("Email", text: $email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                SecureField("Password", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                Button("Sign In") {
-                    // Sign in functionality to be implemented
+            ScrollView {
+                VStack(spacing: 30) {
+                    Text("Welcome Back")
+                        .font(.system(size: 32, weight: .bold))
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $email, placeholder: "Email", imageName: "envelope")
+                    CustomTextField(text: $password, placeholder: "Password", imageName: "lock", isSecure: true)
+                    
+                    Button("Sign In") {
+                        // Sign in functionality to be implemented
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.white)
+                    .foregroundColor(.black)
+                    .cornerRadius(12)
+                    
+                    NavigationLink("Don't have an account? Sign Up", destination: SignUpView())
+                        .foregroundColor(.white)
                 }
-                .padding()
-                .background(Color.white)
-                .foregroundColor(.black)
-                .cornerRadius(10)
+                .padding(.horizontal, 30)
             }
-            .padding()
         }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: backButton)
+    }
+    
+    private var backButton: some View {
+        Button(action: { presentationMode.wrappedValue.dismiss() }) {
+            Image(systemName: "chevron.left")
+                .foregroundColor(.white)
+                .imageScale(.large)
+        }
+    }
+}
+
+struct CustomTextField: View {
+    @Binding var text: String
+    let placeholder: String
+    let imageName: String
+    var isSecure: Bool = false
+    
+    var body: some View {
+        HStack {
+            Image(systemName: imageName)
+                .foregroundColor(.white)
+            if isSecure {
+                SecureField(placeholder, text: $text)
+            } else {
+                TextField(placeholder, text: $text)
+            }
+        }
+        .padding()
+        .background(Color.white.opacity(0.2))
+        .cornerRadius(12)
+        .foregroundColor(.white)
     }
 }
 
--- fitlife/fitlife/Views/SignUpView.swift
+++ fitlife/fitlife/Views/SignUpView.swift
@@ -7,34 +7,46 @@ struct SignUpView: View {
     @State private var email = ""
     @State private var password = ""
     @State private var confirmPassword = ""
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         ZStack {
             GradientBackground()
             
-            VStack(spacing: 30) {
-                Text("Create Account")
-                    .font(.system(size: 32, weight: .bold))
-                    .foregroundColor(.white)
-                
-                TextField("Email", text: $email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                SecureField("Password", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                SecureField("Confirm Password", text: $confirmPassword)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                Button("Sign Up") {
-                    // Sign up functionality to be implemeted later
+            ScrollView {
+                VStack(spacing: 30) {
+                    Text("Create Account")
+                        .font(.system(size: 32, weight: .bold))
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $email, placeholder: "Email", imageName: "envelope")
+                    CustomTextField(text: $password, placeholder: "Password", imageName: "lock", isSecure: true)
+                    CustomTextField(text: $confirmPassword, placeholder: "Confirm Password", imageName: "lock", isSecure: true)
+                    
+                    Button("Sign Up") {
+                        // Sign up functionality to be implemeted later
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.white)
+                    .foregroundColor(.black)
+                    .cornerRadius(12)
+                    
+                    NavigationLink("Already have an account? Log In", destination: SignInView())
+                        .foregroundColor(.white)
                 }
-                .padding()
-                .background(Color.white)
-                .foregroundColor(.black)
-                .cornerRadius(10)
+                .padding(.horizontal, 30)
             }
-            .padding()
+        }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: backButton)
+    }
+    
+    private var backButton: some View {
+        Button(action: { presentationMode.wrappedValue.dismiss() }) {
+            Image(systemName: "chevron.left")
+                .foregroundColor(.white)
+                .imageScale(.large)
         }
     }
 }
