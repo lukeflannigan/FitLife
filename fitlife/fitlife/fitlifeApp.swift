6a429b0172f0a04a15f4b836b9cbc0d43fe6f8c3
gciaburri <gabriel@ciaburribrand.com>
2024-10-01 17:47:30 -0500

    CTP-38-2 Resolve merge conflict for pull request

--- fitlife/fitlife/fitlifeApp.swift
+++ fitlife/fitlife/fitlifeApp.swift
@@ -6,23 +6,26 @@
 //
 
 import SwiftUI
-import SwiftData
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
-struct fitlifeApp: App {
-    let modelContainer: ModelContainer
-    init() {
-        do {
-            modelContainer = try ModelContainer(for: UserGoals.self)
-        } catch {
-            fatalError("Could not initialize ModelContainer")
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
-    
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .modelContainer(modelContainer)
-        }
-    }
+  }
 }
