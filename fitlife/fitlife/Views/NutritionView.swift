4001b41a8672ed6d210c1d08d3d2d9e1e4e81d51
Luke Flannigan <nym15@txstate.edu>
2024-10-03 09:59:58 -0500

    CTP-27 Created basic structure for NutritionView

--- fitlife/fitlife/Views/NutritionView.swift
+++ fitlife/fitlife/Views/NutritionView.swift
@@ -5,14 +5,31 @@ import SwiftUI
 
 struct NutritionView: View {
     var body: some View {
-        VStack {
-            Text("Nutrition View")
-                .font(.custom("Poppins-Bold", size: 24))
-                .padding()
-            Spacer()
+        ScrollView {
+            VStack(spacing: 20) {
+                headerSection
+                // Will need to add more here eventually
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            .padding(.bottom, 80) // To account for tab bar.
         }
         .background(Color(UIColor.systemBackground))
         .edgesIgnoringSafeArea(.bottom)
+    }
+
+    private var headerSection: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Nutrition")
+                    .font(.custom("Poppins-Bold", size: 28))
+                    .foregroundColor(.primary)
+                Text("Track your daily intake")
+                    .font(.custom("Poppins-Regular", size: 16))
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+        }
     }
 }
 
