4a78f07f766dc0e92b306ef4c803a200b9d99f2f
Thomas M <zaz22@txstate.edu>
2024-10-17 00:41:23 -0500

    SCRUM-55 updated Exercise data model to include a difficulty indicator

--- fitlife/fitlife/Models/Exercise.swift
+++ fitlife/fitlife/Models/Exercise.swift
@@ -8,26 +8,27 @@
 import Foundation
 import SwiftData
 
+enum Difficulty: String, CaseIterable, Codable{
+    case easy = "Warmup"
+    case medium = "Moderate"
+    case hard = "Intense"
+}
+
 @Model
 class Exercise: Identifiable, ObservableObject {
     var id: UUID
     var name: String
     var type: String
     var muscleGroup: String
+    var difficulty: Difficulty
 
-    init(id: UUID = UUID(), name: String = "", type: String = "", muscleGroup: String = "") {
+    init(id: UUID = UUID(), name: String = "", type: String = "", muscleGroup: String = "", difficulty: Difficulty = .easy) {
         self.id = id
         self.name = name
         self.type = type
         self.muscleGroup = muscleGroup
+        self.difficulty = difficulty
     }
 }
 
-extension Exercise {
-    static var mockExercises = [
-        Exercise(name: "Bench Press", type: "Strength", muscleGroup: "Chest"),
-        Exercise(name: "Squat", type: "Strength", muscleGroup: "Legs"),
-        Exercise(name: "Deadlift", type: "Strength", muscleGroup: "Back")
-    ]
-}
 
