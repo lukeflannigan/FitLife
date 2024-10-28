143678a4ef36c79f83aad395b99403a83b0e813c
Thomas M <zaz22@txstate.edu>
2024-10-27 23:48:31 -0500

    SCRUM-114 workout data model now contains array of exercises

--- fitlife/fitlife/Models/Workout.swift
+++ fitlife/fitlife/Models/Workout.swift
@@ -11,16 +11,16 @@ import SwiftData
 @Model
 class Workout: Identifiable, ObservableObject {
     var id: UUID
-    var exercise: Exercise
+    var exercise: [Exercise]
     var sets: Int
     var reps: Int
     var weight: Double
     var date: Date
     var isFavorite: Bool
 
-    init(id: UUID = UUID(), exercise: Exercise, sets: Int = 0, reps: Int = 0, weight: Double = 0.0, date: Date = Date(), isFavorite: Bool = false) {
+    init(id: UUID = UUID(), exercises: [Exercise], sets: Int = 0, reps: Int = 0, weight: Double = 0.0, date: Date = Date(), isFavorite: Bool = false) {
         self.id = id
-        self.exercise = exercise
+        self.exercise = exercises
         self.sets = sets
         self.reps = reps
         self.weight = weight
@@ -31,8 +31,8 @@ class Workout: Identifiable, ObservableObject {
 
 extension Workout {
     static var mockWorkoutEntries = [
-        Workout(exercise: Exercise(name: "Bench Press", type: "Strength", muscleGroup: "Chest"), sets: 3, reps: 10, weight: 150),
-        Workout(exercise: Exercise(name: "Squat", type: "Strength", muscleGroup: "Legs"), sets: 4, reps: 8, weight: 200)
+        Workout(exercises: [Exercise(name: "Bench Press", type: "Strength", muscleGroup: "Chest")], sets: 3, reps: 10, weight: 150),
+        Workout(exercises: [Exercise(name: "Squat", type: "Strength", muscleGroup: "Legs")], sets: 4, reps: 8, weight: 200)
     ]
 }
 
