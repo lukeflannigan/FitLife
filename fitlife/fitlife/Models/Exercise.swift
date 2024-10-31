2d1429c06916840cf5e729084bb7911ea2d02b71
Luke Flannigan <nym15@txstate.edu>
2024-10-31 12:49:34 -0500

    SCRUM-25 Updated Exercise model to use correct fields for API exercise data

--- fitlife/fitlife/Models/Exercise.swift
+++ fitlife/fitlife/Models/Exercise.swift
@@ -17,7 +17,7 @@ enum Difficulty: String, CaseIterable, Codable{
 
 @Model
 class Exercise: Identifiable, ObservableObject {
-    var id: String  
+    var id: String
     var name: String
     var type: String
     var muscleGroup: String
@@ -25,8 +25,27 @@ class Exercise: Identifiable, ObservableObject {
     var imageName: String
     var difficulty: Difficulty
     var imageData: Data?
+    var primaryMuscles: [String]
+    var secondaryMuscles: [String]
+    var equipment: String?
+    var force: String?
+    var mechanic: String?
 
-    init(id: UUID = UUID(), name: String = "", type: String = "", muscleGroup: String = "",exerciseDescription: String = "", imageName: String = "",  difficulty: Difficulty = .easy, imageData: Data? = nil) {
+    init(
+        id: String = UUID().uuidString,
+        name: String = "",
+        type: String = "",
+        muscleGroup: String = "",
+        exerciseDescription: String = "",
+        imageName: String = "",
+        difficulty: Difficulty = .easy,
+        imageData: Data? = nil,
+        primaryMuscles: [String] = [],
+        secondaryMuscles: [String] = [],
+        equipment: String? = nil,
+        force: String? = nil,
+        mechanic: String? = nil
+    ) {
         self.id = id
         self.name = name
         self.type = type
@@ -35,6 +54,11 @@ class Exercise: Identifiable, ObservableObject {
         self.imageName = imageName
         self.difficulty = difficulty
         self.imageData = imageData
+        self.primaryMuscles = primaryMuscles
+        self.secondaryMuscles = secondaryMuscles
+        self.equipment = equipment
+        self.force = force
+        self.mechanic = mechanic
     }
 }
 
