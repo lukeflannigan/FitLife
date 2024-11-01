30825316e91b7dea519be26cbaca0d64262740f8
Sam Arshad <jsb211@txstate.edu>
2024-10-31 23:02:32 -0500

    SCRUM 101: Added UI for Daily Intake.
    - Added code to Daily Intake and worked on the UI for it.
    - Added a big green button for it.
     - The Big Green Button is in the middle of the screen with a '+' sign preceding the text.
    
    Going to Add Preview next.

--- fitlife/fitlife/AddAndSaveFoodEntries.swift
+++ fitlife/fitlife/AddAndSaveFoodEntries.swift
@@ -8,7 +8,63 @@
 import SwiftUI
 import Foundation
 
-struct AddFoodEntry{
+struct AddFoodEntryView: View{
+    @State private var name: String = ""   //As in, what is the name of the food item you ate?
+    @State private var calories: String = ""
+    @State private var protein: String = ""
+    @State private var carbs: String = ""
+    @State private var fats: String = ""
+    
+    
+    
+    var body: some View{
+        var body: some View {
+            Form {
+                Section(header: Text("Log Your Food Intake").font(.title3)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 10)) {
+                        TextField("What did you eat?", text: $name)
+                        TextField("Calories", text: $calories)
+                            .keyboardType(.decimalPad)
+                        TextField("Protein (g)", text: $protein)
+                            .keyboardType(.decimalPad)
+                        TextField("Carbohydrates (g)", text: $carbs)
+                            .keyboardType(.decimalPad)
+                        TextField("Fats (g)", text: $fats)
+                            .keyboardType(.decimalPad)
+                    }
+                
+                Section {
+                    Button(action: {
+                        addFoodEntry()
+                    }) {
+                        HStack {
+                            Image(systemName: "plus")
+                            Text("Add Entry")
+                                .fontWeight(.semibold)
+                                .padding(.leading, 5)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                    }
+                    .buttonStyle(.plain)
+                    .listRowBackground(Color.clear) // Remove extra styling
+                }
+            }
+            .navigationTitle("Add Food Entry")
+        }
+    }
+    
+    
+    
+    
+    
+    
     
 }
+        
+    
 
