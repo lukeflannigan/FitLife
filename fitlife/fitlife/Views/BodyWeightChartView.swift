77f27fa4464e5968635cdd64706c210aed7fbc98
Gabriel Ciaburri <aff38@txstate.edu>
2024-11-05 15:19:27 -0600

    SCRUM-119 added bodyweight function and chart

--- fitlife/fitlife/Views/BodyWeightChartView.swift
+++ fitlife/fitlife/Views/BodyWeightChartView.swift
@@ -6,24 +6,46 @@
 //
 
 import SwiftUI
+import Charts
+import SwiftData
 
-struct BodyWeightChartView: View {
-    @Environment(\.modelContext) var modelContext
-    var body: some View {
-        VStack {
-            HStack {
-                Text("Start")
-                Spacer()
-                Text("Current")
-                Spacer()
-                Text("Goal")
-                Spacer()
-                Text("Change")
-            }
+struct WeightChartView: View {
+    @Query var userGoals: [UserGoals]
+        var bodyWeightEntries: [BodyWeightEntry] {
+            userGoals.first?.bodyMetrics.bodyWeightLog ?? []
         }
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Weight")
+                .font(.custom("Poppins-SemiBold", size: 18))
+                .foregroundColor(.primary)
+            
+            Chart {
+                ForEach(bodyWeightEntries) { entry in
+                    LineMark(
+                        x: .value("Date", entry.date),
+                        y: .value("Weight", entry.weight)
+                    )
+                    .foregroundStyle(Color("GradientStart"))
+                }
+            }
+            .frame(height: 150)
+            .chartYAxis {
+                AxisMarks(position: .leading) {
+                    AxisGridLine()
+                    AxisValueLabel()
+                }
+            }
+            .chartXAxis {
+                AxisMarks(position: .bottom) { value in
+                    AxisGridLine()
+                    AxisValueLabel(format: .dateTime.month().day())
+                }
+            }
+            .padding(.top, 10)
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 10).fill(Color(UIColor.systemGray6)))
+        .padding(.bottom, 20)
     }
-}
-
-#Preview {
-    BodyWeightChartView()
 }
