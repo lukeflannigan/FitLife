55b8ba600fa37dbe13c36bade015b8c22a43d1e8
Gabriel Ciaburri <aff38@txstate.edu>
2024-11-14 15:41:07 -0600

    SCRUM-131 Current Workout implemented

---
+++ fitlife/fitlife/Views/ExerciseSetView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseSetView.swift
+//  fitlife
+//
+//  Created by Gabriel Ciaburri on 11/14/24.
+//
+
+import SwiftUI
+
+struct ExerciseSetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseSetView()
+}
