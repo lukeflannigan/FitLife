c677c083a3a26fcc8ca9559a3a2287509630926d
Sam Arshad <jsb211@txstate.edu>
2024-11-20 12:49:14 -0600

    SCRUM 123: Created Encoded Query
    - Created a URL safe encoded ingredients query.
    - Constructed the URL endpoint.
      - It was different than Recipe. Had to figure that out.
      - I read the API documentation (i feel so grown up, I'm gonna make it a greater habit of reading documentation)
    - TESTED IT IN POSTMAN AND IT WORKED.
      - Pardon the Unprofessionalism (Ugly Crying tears of joy)

--- fitlife/fitlife/FoodAPI.swift
+++ fitlife/fitlife/FoodAPI.swift
@@ -83,11 +83,32 @@ struct FoodResponse: Codable{
 class FoodViewModel: ObservableObject{
     @Published var foodItem: [FoodObject] = [] //Array to Hold Fetched a Food's Data
     
-    func fetchData(query: String){
+    func fetchData(ingredient: String){
         if let path = Bundle.main.path(forResource: "FoodSearch", ofType: "plist"),
            let dict = NSDictionary(contentsOfFile: path) as? [String: Any],
-           
+           let id = dict["app_id"] as? String,
+           let key = dict["app_key"] as? String {
+            
+            //     Ensure the query is URL-safe
+            let encodedIngredient = ingredient.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? " "
+            
+            
+            //Construct the URL with the Credentials
+            
+            //let URLString = "https://api.edamam.com/api/food-database/v2/parser?ingr=\(encodedQuery)&app_key=\(key)&_cont="
+            let urlString = "https://api.edamam.com/api/food-database/v2/parser?ingr=\(encodedIngredient)&app_id=\(id)&app_key=\(key)&nutrition-type=logging"
+            print("Constructed URL: \(urlString)")
+            
+            
+            
+            
+        }
+        // Construct the URL string with credentials
+        //            let urlString = "https://api.edamam.com/api/recipes/v2?q=\(encodedQuery)&app_key=\(key)&_cont=CHcVQBtNNQphDmgVQntAEX4BYUt6AwAPSmBJAmEVY1FzAQYVX3cUC2YWMFJ6BldUFzBECmUaMl1zUAUEQzYRUmMXYAYlARFqX3cWQT1OcV9xBE4%3D&type=any&app_id=\(id)"
+        //
+        //            //}
+        //        }
+        //    }
     }
 }
-
 
