fd4ad3da4109ac350aaad191e50296d87f7aa1a5
Thomas M <zaz22@txstate.edu>
2024-11-25 15:21:50 -0600

    SCRUM-126 created workout template view + added ui element navigation

---
+++ fitlife/fitlife/Views/WorkoutTemplateView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutTemplateView.swift
+//  fitlife
+//
+//  Created by Thomas Mendoza on 11/25/24.
+//
+
+import SwiftUI
+
+struct WorkoutTemplateView: View{
+    var body: some View{
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    WorkoutTemplateView()
+}
